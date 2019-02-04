793474376b733cadfc41016e70884b964bc8c54e
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-02-04 13:36:35 +0200

    routes for usageReports

---
+++ controllers/usages.js
@@ -0,0 +1,30 @@
+const usageRouter = require('express').Router()
+const Usage = require('../models/usage')
+
+usageRouter.get('/', async (req, res) => {
+    const usages = await Usage.find({}).populate('group', { _id: 1, name: 1 })
+    res.json(usages.map(Usage.format))
+})
+
+usageRouter.post('/', async (req, res) => {
+    try {
+        const body = req.body
+        if (body.group.length === 0) {
+            return res.status(400).json({error: 'No group selected'})
+        }
+        const usage = new Usage({
+            date: new Date(),
+            group: body.group,
+            under: body.under,
+            over: body.over,
+            other: body.other,
+        })
+
+        const savedUsage = await usage.save()
+
+        res.json(Usage.format(savedUsage))
+    } catch (exception) {
+        console.log(exception)
+        res.status(500).json({error: 'something went wrong...'})
+    }
+})
